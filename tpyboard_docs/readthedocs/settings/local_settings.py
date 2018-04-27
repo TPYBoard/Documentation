88e7569e2eed90584634477e214fefdb4d6c3880
TPYBoard <info@turnipsmart.com>
2018-04-27 15:08:29 +0800

    ADD

--- tpyboard_docs/readthedocs/settings/local_settings.py
+++ tpyboard_docs/readthedocs/settings/local_settings.py
@@ -4,6 +4,6 @@ import os
 SITE_ROOT = '/'.join(os.path.dirname(__file__).split('/')[0:-2])
 
 TEMPLATE_DIRS = (
-    "%s/templates/" % SITE_ROOT, # Your custom template directory, before the RTD one to override it.
+    # "%s/templates/" % SITE_ROOT, # Your custom template directory, before the RTD one to override it.
     "%s/readthedocs/templates/" % SITE_ROOT, # Default RTD template dir
 )
